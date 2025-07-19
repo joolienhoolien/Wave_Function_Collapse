6a935cf5a55399f009071114974dc9cbbb7d2ca8
joolienhoolien <julienpecquet@gmail.com>
2025-07-19 00:18:51 -0600

    added drawing to a window and created a grid of tiles

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import sys
-
 import pygame
 from pygame.locals import *
 
@@ -12,7 +11,7 @@ GREEN = pygame.Color(0,255,0)
 BLUE = pygame.Color(0,0,255)
 
 #Display
-SCREEN_WIDTH, SCREEN_HEIGHT = 400, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 100, 100
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 
@@ -32,7 +31,8 @@ BLANK, UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3, 4
 
 class Tile(pygame.sprite.Sprite):
     collapsed = False
-    options: [BLANK, UP, RIGHT, DOWN, LEFT]
+    options = [BLANK, UP, RIGHT, DOWN, LEFT]
+    value = BLANK
 
     def __init__(self, x, y, tile):
         super().__init__()
@@ -43,6 +43,30 @@ class Tile(pygame.sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
+    def set_value(self, value):
+        self.value = value
+        self.collapsed = True
+        self.options = [value]
+        self.image = pygame.image.load(tiles[value])
+
+
+def setup():
+    global grid
+    grid = [[0 for _ in range(grid_dim)] for _ in range(grid_dim)]
+    for i in range(grid_dim):
+        for j in range(grid_dim):
+            tile_width = SCREEN_WIDTH / grid_dim
+            tile_x_center = tile_width / 2
+            x = tile_x_center + (tile_width * i)
+
+            tile_width = SCREEN_WIDTH / grid_dim
+            tile_y_center = tile_width / 2
+            y = tile_y_center + (tile_width * j)
+
+            grid[i][j] = Tile(x, y, BLANK)
+
+
+
 def game_loop():
     while True:
         for event in pygame.event.get():
@@ -50,12 +74,15 @@ def game_loop():
                 pygame.quit()
                 sys.exit()
 
-        T1.draw(DISPLAYSURF)
+        for row in grid:
+            for tile in row:
+                tile.draw(DISPLAYSURF)
         pygame.display.update()
         FramePerSec.tick(FPS)
 
 
 if __name__ == "__main__":
     pygame.init()
-    T1 = Tile(0, 0, BLANK)
+    setup()
+    #grid[1][0].set_value(LEFT) #manually collapsing cell for testing
     game_loop()
